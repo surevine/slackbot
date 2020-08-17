0ff53601f6cd96be21337cc97ee87990189a944a
Ben <57803527+Ben-random@users.noreply.github.com>
2020-08-17 13:31:37 +0100

    Update lambda_function.py
    Responds with the user name

--- lambda_function.py
+++ lambda_function.py
@@ -8,8 +8,12 @@ def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
     logger.info('## EVENT')
-    logger.info(event)
-    body = {"Request " : event["body"] }
+    logger.info(json.dumps(event))
+    x = event["body"].find('user_name')
+    y = event["body"].find("&", x + 10, x + 35)
+    text = event["body"]
+    user_name = text[x + 10 : y]
+    body = {"Hello " : user_name }
     return {
         'statusCode': 200,
         'body': json.dumps(body)
