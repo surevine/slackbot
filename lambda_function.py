93c3604879c16584ce6b0e6fd7673b0d69ab810b
Ben <57803527+Ben-random@users.noreply.github.com>
2020-08-21 11:32:59 +0100

    Fixed bugs
    One word or no text command calls no longer crash the program slack side

--- lambda_function.py
+++ lambda_function.py
@@ -6,78 +6,89 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+CONFLUENCE = "i-03c9b93a5e590d8df"
+VPN = "i-010e9db190ec434d4"
+GIT = "i-0241c0a8ea30ecf46"
+DATABASE = "i-081ffee71f6238ad0"
+AUTH_SERVICE = "i-0e690e2ee6448f38a"
+
+groups = {
+        "confluence": CONFLUENCE,
+        "vpn": VPN,
+        "git": GIT,
+        "database": DATABASE,
+        "auth service": AUTH_SERVICE
+    }
+    
+region = "eu-west-2"
+    
+ec2 = boto3.client("ec2", region_name=region)
+    
 def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
     logger.info('## EVENT')
     logger.info(json.dumps(event))
-    region = "eu-west-2"
-    instances = {
-        "confluence": "i-03c9b93a5e590d8df",
-        "vpn": "i-010e9db190ec434d4",
-        "git": "i-0241c0a8ea30ecf46",
-        "database": "i-081ffee71f6238ad0",
-        "auth service": "i-0e690e2ee6448f38a"
-    }
-    ec2 = boto3.client("ec2", region_name=region)
     
-    if "body" not in event:
+    try:    
+        command = get_command(event)
+        operation = command[0]
+        target = command[1]
+        response = perform_operation(operation, target)
+    except Exception as e:
+        body = { "Response" : str(e) }
         return {
-            'statusCode': 500,
-            "Response" : "Error, required information missing"
+            'statusCode': 200,
+            "body" : json.dumps(body)
         }
-        
-    body = event["body"]
-    qs = urllib.parse.parse_qs(body)
-    
-    if "text" not in qs:
-        return {
-            'statusCode': 400,
-            "Response" : "Error, required information missing"
-        }
-    try:
-        text = qs["text"]
-        text = text[0]
-        split_command = text.lower().split()
-        operation = split_command[0]
-        target = split_command[1]
-    except:
-        return {
-            "statusCode" : 400,
-            "Response" : "Error, required information missing"
-        }
-    def start(target):
-        try:
-            ec2.start_instances(InstanceIds = [instances[target]])
-            return "Starting " + target + "..."
-        except:
-            return {
-                'statusCode': 404,
-                'Response': "Service not found"
-            }
-
-    def stop(target):
-        try:
-            ec2.stop_instances(InstanceIds = [instances[target]])
-            return "Stopping " + target + "..."
-        except:
-            return {
-                'statusCode': 404,
-                'Response': "Service not found"
-            }
-            
-    if operation == "start":
-        response = start(target)
-    elif operation == "stop":
-        response = stop(target)
-    else:
-        return {
-        'statusCode': 404,
-        'Response': "Command not recognised"
-    }
     
     body = { "Response" : response }
     return {
         'statusCode': 200,
         'body': json.dumps(body)
     }
+
+def get_command(event):
+    if "body" not in event:
+        raise Exception("Error, required information missing")
+        
+    body = event["body"]
+    qs = urllib.parse.parse_qs(body)
+    
+    if "text" not in qs:
+        raise Exception("Error, required information missing")
+    
+    try:
+        text = qs["text"]
+        text = text[0]
+        split_command = text.lower().split()
+    except:
+        raise Exception("Error, required information missing")
+        
+    if len(split_command) != 2:
+        raise Exception("Error, requires two words")
+        
+    return split_command
+
+def perform_operation(operation, target):
+    if operation == "start":
+        response = start(target)
+    elif operation == "stop":
+        response = stop(target)
+    else:
+        raise Exception("Command not recognised")
+    return response
+    
+def start(target):
+    try:
+        ec2.start_instances(InstanceIds = [groups[target]])
+        return "Starting " + target + "..."
+    except:
+        raise Exception("Service not found")
+    
+def stop(target):
+    try:
+        ec2.stop_instances(InstanceIds = [groups[target]])
+        return "Stopping " + target + "..."
+    except:
+        raise Exception("Service not found")
