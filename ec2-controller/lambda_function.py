286e75b25b730173a1b3e9beec90c0322681a9e4
[ben-random] <[Benjamin.girvan@icloud.com]>
2020-08-24 15:58:08 +0100

    Separated EC2 from slack

--- ec2-controller/lambda_function.py
+++ ec2-controller/lambda_function.py
@@ -11,6 +11,19 @@ logger.setLevel(logging.INFO)
 region = "eu-west-2"
     
 ec2 = boto3.client("ec2", region_name=region)
+
+CONFLUENCE = "i-03c9b93a5e590d8df"
+VPN = "i-010e9db190ec434d4"
+GIT = "i-0241c0a8ea30ecf46"
+DATABASE = "i-081ffee71f6238ad0"
+AUTH_SERVICE = "i-0e690e2ee6448f38a"
+
+groups = {
+        "confluence": [DATABASE, AUTH_SERVICE, VPN, CONFLUENCE],
+        "vpn": [VPN],
+        "git": [VPN, DATABASE, GIT]
+    }
+
     
 def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
@@ -18,4 +31,41 @@ def lambda_handler(event, context):
     logger.info('## EVENT')
     logger.info(json.dumps(event))
     
+    try:    
+        response = perform_operation("start", "git")
+    except Exception as e:
+        body = { "Response" : str(e) }
+        return {
+            'statusCode': 200,
+            "body" : json.dumps(body)
+        }
+    
+    return {
+        "response": response
+    }
+    
+def perform_operation(operation, target):
+    if operation == "start":
+        response = start(target)
+    elif operation == "stop":
+        response = stop(target)
+    else:
+        raise Exception("Command not recognised")
+    return response
+    
+def start(target):
+    try:
+        ec2.start_instances(InstanceIds = groups[target])
+        return "Starting " + target + "..."
+    except Exception as e:
+        logger.error(e)
+        raise Exception("failed to start")
+        
+    
+def stop(target):
+    try:
+        ec2.stop_instances(InstanceIds = groups[target])
+        return "Stopping " + target + "..."
+    except:
+        raise Exception("Service not found")
    
--- slack-interface/lambda_function.py
+++ slack-interface/lambda_function.py
@@ -8,22 +8,6 @@ import hashlib, hmac
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-CONFLUENCE = "i-03c9b93a5e590d8df"
-VPN = "i-010e9db190ec434d4"
-GIT = "i-0241c0a8ea30ecf46"
-DATABASE = "i-081ffee71f6238ad0"
-AUTH_SERVICE = "i-0e690e2ee6448f38a"
-
-groups = {
-        "confluence": [DATABASE, AUTH_SERVICE, VPN, CONFLUENCE],
-        "vpn": [VPN],
-        "git": [VPN, DATABASE, GIT]
-    }
-    
-region = "eu-west-2"
-    
-ec2 = boto3.client("ec2", region_name=region)
-    
 def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
@@ -41,7 +25,21 @@ def lambda_handler(event, context):
         command = get_command(event)
         operation = command[0]
         target = command[1]
-        response = perform_operation(operation, target)
+        
+        lambda_client = boto3.client('lambda')
+
+   
+        inputParams = {
+            "operation"   : operation,
+            "target"      : target
+        }
+        response = lambda_client.invoke(
+            FunctionName = os.environ["EC2_CONTROLLER_FUNCTION_ARN"],
+            InvocationType = 'RequestResponse',
+            Payload = json.dumps(inputParams)
+        )
+        response = json.load(response["Payload"])
+
     except Exception as e:
         body = { "Response" : str(e) }
         return {
@@ -98,28 +96,3 @@ def get_command(event):
         raise Exception("Error, requires two words")
         
     return split_command
-
-def perform_operation(operation, target):
-    if operation == "start":
-        response = start(target)
-    elif operation == "stop":
-        response = stop(target)
-    else:
-        raise Exception("Command not recognised")
-    return response
-    
-def start(target):
-    try:
-        ec2.start_instances(InstanceIds = groups[target])
-        return "Starting " + target + "..."
-    except Exception as e:
-        logger.error(e)
-        raise Exception("failed to start")
-        
-    
-def stop(target):
-    try:
-        ec2.stop_instances(InstanceIds = groups[target])
-        return "Stopping " + target + "..."
-    except:
-        raise Exception("Service not found")
