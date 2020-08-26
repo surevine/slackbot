d802edab5d22be7270a146d5d68821de5befe550
[ben-random] <[Benjamin.girvan@icloud.com]>
2020-08-26 11:02:42 +0100

    ids no longer hard-coded into lambda

--- ec2-controller/lambda_function.py
+++ ec2-controller/lambda_function.py
@@ -4,71 +4,83 @@ import logging
 import urllib
 import boto3
 import time
-import hashlib, hmac
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-region = "eu-west-2"
-    
-ec2 = boto3.client("ec2", region_name=region)
+ec2 = boto3.resource("ec2")
 
-CONFLUENCE = "i-03c9b93a5e590d8df"
-VPN = "i-010e9db190ec434d4"
-GIT = "i-0241c0a8ea30ecf46"
-DATABASE = "i-081ffee71f6238ad0"
-AUTH_SERVICE = "i-0e690e2ee6448f38a"
-
-groups = {
-        "confluence": [DATABASE, AUTH_SERVICE, VPN, CONFLUENCE],
-        "vpn": [VPN],
-        "git": [VPN, DATABASE, GIT]
-    }
-
-    
 def lambda_handler(event, context):
-    logger.info('## ENVIRONMENT VARIABLES')
-    logger.info(os.environ)
-    logger.info('## EVENT')
-    logger.info(json.dumps(event))
-    
-    operation = event["operation"]
-    target = event["target"]
 
-    try:    
-        response = perform_operation(operation, target)
-    except Exception as e:
-        body = { "Response" : str(e) }
-        return {
-            'statusCode': 200,
-            "body" : json.dumps(body)
-        }
+    logger.info(event)
+    operation = event["operation"]
+    service_name = event["target"]
+
+    target_instance = get_instance_by_name(service_name)
+
+    if target_instance is None:
+        return { "response": "Service not found" }
+
+    print("Operation: " + operation + " Service: " + service_name + " Instance: " + target_instance.id)
+
+    for tag in target_instance.tags:
+
+        if 'dependants' in tag['Key']:
+            dependant_names = [x.strip() for x in tag['Value'].split(',')]
+            dependant_instances = get_instances_by_name(dependant_names)
+
+            for dependant in dependant_instances:
+                perform_operation(operation, dependant)
+
+    perform_operation(operation, target_instance)
+
+    return {  "response": "Success" }
+
+
+def get_instance_by_name(instance_name):
+    return next(iter(ec2.instances.filter(Filters=get_name_filter([instance_name]))), None)
+
+
+def get_instances_by_name(instance_name_list):
+    return ec2.instances.filter(Filters=get_name_filter(instance_name_list))
+
+
+def get_name_filter(name_list):
+    return [{
+        'Name':'tag:Name', 
+        'Values': name_list}]
     
-    return {
-        "response": response
-    }
+   
+def perform_operation(operation, instance):
     
-def perform_operation(operation, target):
-    if operation == "start":
-        response = start(target)
-    elif operation == "stop":
-        response = stop(target)
-    else:
-        raise Exception("Command not recognised")
-    return response
-    
-def start(target):
     try:
-        ec2.start_instances(InstanceIds = groups[target])
-        return "Starting " + target + "..."
+        if operation == "start":
+            response = start(instance)
+        elif operation == "stop":
+            response = stop(instance)
+        else:
+            response = "Command not recognised"
+    except Exception as e:
+        logger.error(e)
+        response = "Failed to " + operation + " instance " + instance.id
+
+    return response
+
+
+def start(instance):
+    try:
+        instance.start()
+        logger.info("Starting " + instance.id)
+        logger.info(instance.tags)
     except Exception as e:
         logger.error(e)
         raise Exception("failed to start")
-        
-    
-def stop(target):
+
+
+def stop(instance):
     try:
-        ec2.stop_instances(InstanceIds = groups[target])
-        return "Stopping " + target + "..."
+        instance.stop()
+        logger.info("Stopping " + instance.id)
+        logger.info(instance.tags)
     except:
         raise Exception("Service not found")
    
